aeaacff120e88a579a8c2bf4494a3f0742ccc9ce
NguyenNgocQuangMinh <quangminh0512@icloud.com>
2023-04-07 17:47:13 +0700

    dropdown-time

--- src/components/Calendar/CreatePlan.js
+++ src/components/Calendar/CreatePlan.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import "./CreatePlan.css"
 import { IoLocationSharp } from "react-icons/io5"
 import { BsCurrencyEuro, BsFillPersonFill } from "react-icons/bs"
@@ -6,21 +6,58 @@ import { FaRegClock, FaTags } from "react-icons/fa"
 import { AiOutlinePlus } from "react-icons/ai"
 import { DayContext } from "../../layouts/Calendar/CalendarLayout"
 import moment from "moment"
+import BasicCalendar from "./BasicCalendar"
 const CreatePlan = ({ remove }) => {
-	const currentDay = useContext(DayContext)
-	const nowDay = moment(currentDay).format("YYYY/MM/DD")
+	const [displayDayPicker, setDisplayDayPicker] = useState(false)
+	const [dayPicker, setDayPicker] = useState(new Date())
+	// const hours = dayPicker.getHours().toString().padStart(2, "0")
+	// const minutes = dayPicker.getMinutes().toString().padStart(2, "0")
+	// const currentTime = `${hours}:${minutes}`
+	const [timePicker, setTimePicker] = useState("00:00 - 01:00")
+	// const nowDay = moment(dayPicker).format("YYYY/MM/DD")
+	const showDayPicker = () => {
+		setDisplayDayPicker(!displayDayPicker)
+	}
+	// Loop through all hours of the day
+	const arrayTime = Array.from({ length: 24 }, (v, i) => {
+		const startTime = new Date()
+		startTime.setHours(i, 0, 0)
+		const endTime = new Date(startTime)
+		endTime.setHours(endTime.getHours() + 1)
+		const formattedStartTime =
+			startTime.getHours().toString().padStart(2, "0") +
+			":" +
+			startTime.getMinutes().toString().padStart(2, "0")
+		const formattedEndTime =
+			endTime.getHours().toString().padStart(2, "0") +
+			":" +
+			endTime.getMinutes().toString().padStart(2, "0")
+		const intervalTime = formattedStartTime + " - " + formattedEndTime
+		return {
+			id: i + 1,
+			time: intervalTime,
+		}
+	})
 
-	const getDayName = (date = currentDay, locale = "vi-VN") => {
+	const getDayName = (date = dayPicker, locale = "vi-VN") => {
 		return date.toLocaleDateString(locale, { weekday: "long" })
 	}
 
-	const day = currentDay.getDate()
-	const month = currentDay.getMonth()
+	const day = dayPicker.getDate()
+	const month = dayPicker.getMonth()
 	const handleCancel = () => {
 		remove()
 	}
 	return (
 		<div className="create-plan-container">
+			<div className="calendar-picker-container">
+				{displayDayPicker && (
+					<BasicCalendar
+						onChange={setDayPicker}
+						value={dayPicker}
+					/>
+				)}
+			</div>
 			<div className="create-plan-box">
 				<div className="create-plan-btn">Tạo kế hoạch</div>
 				<input
@@ -69,7 +106,9 @@ const CreatePlan = ({ remove }) => {
 						<FaRegClock size={30} />
 					</div>
 					<div className="date-detail-create-plan-box">
-						<div className="date-detail-create-plan-container">
+						<div
+							onClick={showDayPicker}
+							className="date-detail-create-plan-container">
 							{getDayName()}, {day} tháng {month}{" "}
 						</div>
 						<div className="toggle-date-create-plan-container">
@@ -83,9 +122,20 @@ const CreatePlan = ({ remove }) => {
 						</div>
 						<div className="time-detail-create-plan-container">
 							{" "}
-							19:00 - 21:00
+							<p>{timePicker}</p>
+							<div className="dropdown-time-detail-create-plan-container">
+								{arrayTime.map((time) => {
+									return (
+										<div
+											onClick={() => setTimePicker(time.time)}
+											key={time.id}
+											className="dropdown-time-detail-create-plan-box">
+											{time.time}
+										</div>
+									)
+								})}
+							</div>
 						</div>
-
 						<div className="repeat-create-plan-container"> Lặp lại</div>
 					</div>
 				</div>
