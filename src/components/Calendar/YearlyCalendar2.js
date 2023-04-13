d2045e0b433344453089039c768bbcad14d70696
NguyenNgocQuangMinh <quangminh0512@icloud.com>
2023-04-13 16:39:25 +0700

    commit

--- src/components/Calendar/YearlyCalendar2.js
+++ src/components/Calendar/YearlyCalendar2.js
@@ -1,51 +1,16 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { IoCaretBackOutline } from "react-icons/io5"
 import { TbPlayerTrackNextFilled } from "react-icons/tb"
 const YearlyCalendar2 = () => {
 	const currentDate = new Date()
 	let currentYear = currentDate.getFullYear()
-	let currentMonth = currentDate.getMonth() + 12
-	const nextMonth = 0
+	let currentMonth = currentDate.getMonth()
+
 	// generate day of week
 	const dayInWeek = ["CN", "T2", "T3", "T4", "T5", "T6", "T7"]
 
 	// get the last date of the previous month
 	const lastDateOfMonth = new Date(currentYear, currentMonth, 0).getDate()
-
-	// get the first day of next month
-	const firstDateOfMonth = new Date(currentYear, currentMonth, 1)
-	const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay()
-
-	const firstDayOfMonthString =
-		dayInWeek[firstDayOfMonth] +
-		"," +
-		firstDateOfMonth.toLocaleDateString("en-us", {
-			year: "numeric",
-			month: "numeric",
-			day: "numeric",
-		}) // parse to string
-
-	const paddingDay = dayInWeek.indexOf(firstDayOfMonthString.split(",")[0]) // get padding by index of dayInWeek
-
-	const loopDayOfCurrentMonth = paddingDay + lastDateOfMonth
-
-	const arrayLoopDay = Array.from({ length: loopDayOfCurrentMonth }, (v, i) => {
-		return {
-			id: i + 1,
-		}
-	})
-
-	const test = () => {
-		for (let i = currentMonth; i > 0; i--) {
-			console.log(currentMonth--)
-		}
-	}
-
-	const test2 = () => {
-		for (let i = currentMonth; i < 12; i++) {
-			console.log(currentMonth++)
-		}
-	}
 
 	const arrayDate31 = Array.from({ length: 31 }, (v, i) => {
 		return {
@@ -69,7 +34,7 @@ const YearlyCalendar2 = () => {
 	})
 
 	const months = [
-		{ id: 1, name: "January", date: arrayDate31, paddingDay: 0 },
+		{ id: 1, name: "January", date: arrayDate31 },
 		{
 			id: 2,
 			name: "February",
@@ -127,6 +92,59 @@ const YearlyCalendar2 = () => {
 		},
 	]
 
+	// function calculate padding Date
+	const calcPaddingDay = (monthOfDate) => {
+		// get the first day of next month
+		const firstDateOfMonth = new Date(currentYear, monthOfDate, 1)
+		const firstDayOfMonth = new Date(currentYear, monthOfDate, 1).getDay()
+
+		// parse to string
+		const firstDayOfMonthString =
+			dayInWeek[firstDayOfMonth] +
+			"," +
+			firstDateOfMonth.toLocaleDateString("en-us", {
+				year: "numeric",
+				month: "numeric",
+				day: "numeric",
+			})
+
+		// get padding by index of dayInWeek
+		const paddingDay = dayInWeek.indexOf(firstDayOfMonthString.split(",")[0])
+
+		return paddingDay
+	}
+
+	// function find padding day of each month and push it into array
+	const findPaddingDate = () => {
+		const arrayOfPaddingDate = []
+		for (let i = 0; i < months.length; i++) {
+			const currentMonthId = months[i].id
+			if (currentMonthId < currentMonth) {
+				const prevCurrentMonth = currentMonth - currentMonthId
+				arrayOfPaddingDate.push(calcPaddingDay(prevCurrentMonth))
+			} else if (currentMonthId > currentMonth) {
+				const nextCurrentMonth = currentMonth + currentMonthId
+				arrayOfPaddingDate.push(calcPaddingDay(nextCurrentMonth))
+			} else {
+				arrayOfPaddingDate.push(calcPaddingDay(currentMonth))
+			}
+		}
+		return arrayOfPaddingDate
+	}
+	const paddingDates = findPaddingDate()
+
+	// this function generate an array dates of each month
+	const arrayDateOfMonth = paddingDates.map((paddingDay) => {
+		const dateInMonth = paddingDay + lastDateOfMonth
+		return Array.from({ length: dateInMonth }, (v, i) => {
+			return {
+				id: i + 1,
+				name: i + 1,
+				paddingDate: paddingDay,
+			}
+		})
+	})
+
 	return (
 		<div className="calendar-yearly__container">
 			{/* Hedaer */}
@@ -147,14 +165,14 @@ const YearlyCalendar2 = () => {
 			</div>
 			{/* Body */}
 			<div className="body-calendar__yearly__container">
-				{months.map((calendar) => {
+				{months.map((calendar, index) => {
 					return (
 						<div
 							key={calendar.id}
 							className="each-calendar__yearly__container">
 							<div className="month-calendar__yearly">{calendar.name}</div>
 							<div className="week-of-day__yearly__container">
-								{dayInWeek.map((day, index) => {
+								{dayInWeek.map((day) => {
 									return (
 										<div
 											className="day__yearly"
@@ -165,24 +183,9 @@ const YearlyCalendar2 = () => {
 								})}
 							</div>
 							<div className="date-calendar__yearly__container">
-								{arrayLoopDay.map((date) => {
-									if (date.id > paddingDay) {
-										return (
-											<button
-												key={date.id}
-												className="date__yearly">
-												{date.id - paddingDay}
-											</button>
-										)
-									}
-									return (
-										<button
-											key={date.id}
-											className="padding-date__yearly">
-											hi
-										</button>
-									)
-								})}
+								{arrayDateOfMonth[index].map((element) => (
+									<p key={element.id}>{element.name}</p>
+								))}
 							</div>
 						</div>
 					)
