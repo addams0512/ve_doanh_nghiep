1f6e7bdc97343daf77ec5434ca1b5f2a9e956e75
NguyenNgocQuangMinh <quangminh0512@icloud.com>
2023-04-24 16:42:53 +0700

    commit

--- src/pages/FormMakeMoneyPage.js
+++ src/pages/FormMakeMoneyPage.js
@@ -1,18 +1,18 @@
-import React from "react";
-import Sidebar from "../components/Sidebar";
-import "./FormMakeMoneyPage.css";
+import React from "react"
+import Sidebar from "../components/Sidebar"
+import "./FormMakeMoneyPage.css"
 // import Phonebook from "../layouts/CreateWebsite/Phonebook";
 // import CreateEnterprise from "../layouts/CreateWebsite/CreateEnterprise";
-import Setting from "../layouts/FormMakeMoney/Setting";
+import Setting from "../layouts/FormMakeMoney/Setting"
 const CalendarPages = () => {
-  return (
-    <div className="form-make-money-page">
-      <Sidebar />
-      {/* <CreateEnterprise /> */}
-      {/* <Phonebook /> */}
-      <Setting />
-    </div>
-  );
-};
+	return (
+		<div className="form-make-money-page">
+			<Sidebar />
+			{/* <CreateEnterprise /> */}
+			{/* <Phonebook /> */}
+			<Setting />
+		</div>
+	)
+}
 
-export default CalendarPages;
+export default CalendarPages
